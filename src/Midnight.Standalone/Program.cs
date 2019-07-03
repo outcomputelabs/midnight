f81b21644b21cd075a3d30bce9fa832c7885b543
Jorge Candeias <candeias@fastmail.fm>
2019-07-03 13:26:29 +0100

    Added standalone project

---
+++ src/Midnight.Standalone/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Midnight.Standalone
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(configuration =>
+                {
+                    configuration.AddCommandLine(args);
+                    configuration.AddEnvironmentVariables(nameof(Midnight));
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.Configure<ConsoleLifetimeOptions>(options =>
+                    {
+                        options.SuppressStatusMessages = true;
+                    });
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            return host.RunAsync();
+        }
+    }
+}
