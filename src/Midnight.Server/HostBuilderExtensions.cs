50ec5d0cd0942c355f0809a43b4b30733eed133e
Jorge Candeias <candeias@fastmail.fm>
2019-07-03 13:30:26 +0100

    Added placeholder builder extension

---
+++ src/Midnight.Server/HostBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Midnight.Server
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseMidnight(this IHostBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
